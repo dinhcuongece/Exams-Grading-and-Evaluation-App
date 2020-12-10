ab094e5dc8627790fb48999a64aa11c89ad0d01e
gnouc52977 <gnoucle.5297@gmail.com>
2020-12-11 04:48:25 +0700

    move folder

---
+++ FeatureTesting.py
@@ -0,0 +1,44 @@
+"""""""""""""""
+##### color+opacity
+'import cv2
+
+image1 = cv2.imread('MCQPaper.jpg')
+image = cv2.resize(image1, (500,500), interpolation=cv2.INTER_LINEAR)
+overlay = image.copy()
+
+x, y, w, h = 100, 100, 100, 100  # Rectangle parameters
+cv2.rectangle(overlay, (x, y), (x+w, y+h), (0, 200, 0), -1)  # A filled rectangle
+
+alpha = 1  # Transparency factor.
+
+# Following line overlays transparent rectangle over the image
+image_new = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
+cv2.imshow("test",image_new)
+cv2.waitKey(0) """
+## Mask a logo
+# Load two images
+img1 = cv2.imread('messi5.jpg')
+img2 = cv2.imread('opencv_logo.png')
+
+# I want to put logo on top-left corner, So I create a ROI
+rows,cols,channels = img2.shape
+roi = img1[0:rows, 0:cols ]
+
+# Now create a mask of logo and create its inverse mask also
+img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+# Now black-out the area of logo in ROI
+img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+
+# Take only region of logo from logo image.
+img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+
+# Put logo in ROI and modify the main image
+dst = cv2.add(img1_bg,img2_fg)
+img1[0:rows, 0:cols ] = dst
+
+cv2.imshow('res',img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
